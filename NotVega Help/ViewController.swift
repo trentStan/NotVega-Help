278908aad527b77d9159dfb325f26bb2d142ba3a
trentStan <71011863+trentStan@users.noreply.github.com>
2022-06-20 13:07:22 +0200

    Initial Commit

---
+++ NotVega Help/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotVega Help
+//
+//  Created by IACD-022 on 2022/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
