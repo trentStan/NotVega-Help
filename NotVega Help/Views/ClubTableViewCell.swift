29debe2ddacdbc457f9583d7f298189ccc30dd35
Sibusiso94 <sybog1994@gmail.com>
2022-07-11 15:31:24 +0200

    Complete Club View Controller

---
+++ NotVega Help/Views/ClubTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ClubTableViewCell.swift
+//  NotVega Help
+//
+//  Created by IACD-019 on 2022/07/11.
+//
+
+import UIKit
+
+class ClubTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
