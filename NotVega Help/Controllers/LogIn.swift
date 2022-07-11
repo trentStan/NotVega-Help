d21e0c1a27a2247f426007d4b26659e1e4e08871
Tayla Roach <tayla.roach12@gmail.com>
2022-07-11 13:18:23 +0200

    Added UI to login page

--- NotVega Help/Controllers/LogIn.swift
+++ NotVega Help/Controllers/LogIn.swift
@@ -9,13 +9,27 @@ import UIKit
 import FirebaseFirestore
 import FirebaseAuth
 
+extension UIImage {
+
+    func addImagePadding(x: CGFloat, y: CGFloat) -> UIImage? {
+        let width: CGFloat = size.width + x
+        let height: CGFloat = size.height + y
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: width, height: height), false, 0)
+        let origin: CGPoint = CGPoint(x: (width - size.width) / 2, y: (height - size.height) / 2)
+        draw(at: origin)
+        let imageWithPadding = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return imageWithPadding
+    }
+}
+
 class LogIn: UIViewController {
     
     private let db = Firestore.firestore()
     var window: UIWindow?
     
-    @IBOutlet var ID: UITextField!
-    @IBOutlet var Password: UITextField!
+    @IBOutlet weak var ID: UITextField!
+    @IBOutlet weak var Password: UITextField!
     @IBOutlet var errLabel: UILabel!
     
     
@@ -34,7 +48,35 @@ class LogIn: UIViewController {
         print("login loaded")
         errLabel.alpha = 0
         // Do any additional setup after loading the view.
+        
+        //UI
+        
+        //Adding icon to text fields
+        let IdImage = UIImage(named: "person")
+        let tintedIdImage = IdImage?.withRenderingMode(.alwaysTemplate)
+        addLeftImageTo(txtField: ID, andImage: tintedIdImage!)
+        //ID.tintColor = .gray
+        ID.alpha = 0.5
+        
+        let PasswordImage = UIImage(named: "lockgray")
+        let tintedPasswordImage = PasswordImage?.withRenderingMode(.alwaysTemplate)
+        addLeftImageTo(txtField: Password, andImage: tintedPasswordImage!)
+        //Password.tintColor = .gray
+        Password.alpha = 0.5
+        //
     }
+    
+    //UI
+    
+    //function to add icon to left of text field
+    func addLeftImageTo(txtField: UITextField, andImage img: UIImage) {
+        let leftImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 45, height: 45))
+        leftImageView.image = img.addImagePadding(x: 20, y: 10)
+        txtField.leftView = leftImageView
+        txtField.leftViewMode = .always
+    }
+    
+    //
     
     private func checkAuth(enteredID: String, psw: String) {
         let users = db.collection("Users")
