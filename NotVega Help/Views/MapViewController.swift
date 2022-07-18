f968d1d8cbdf5e8ffa118aa9573afaec9773571f
Tayla Roach <tayla.roach12@gmail.com>
2022-07-18 14:57:15 +0200

    Added campus map. Added app icons. Changed sign out button. Added launch screen.

--- NotVega Help/Views/MapViewController.swift
+++ NotVega Help/Views/MapViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet var campusMapImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
     }
+    
     
 
     /*
