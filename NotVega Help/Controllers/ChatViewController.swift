325c42dd2ae43917ebe3fa1a48aaf02a9dcc0eb5
Sibusiso94 <sybog1994@gmail.com>
2022-07-22 10:41:31 +0200

    Complete Firebase Connection for Messages

---
+++ NotVega Help/Controllers/ChatViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ChatViewController.swift
+//  NotVega Help
+//
+//  Created by IACD-019 on 2022/07/21.
+//
+
+import UIKit
+import FirebaseFirestore
+import FirebaseAuth
+
+
+class ChatViewController: UIViewController {
+    
+    private let db = Firestore.firestore()
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    var messages: [Message] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "ChatCell")
+        
+        loadMessages()
+    }
+    
+    func loadMessages() {
+        
+        db.collection("Messages").order(by: "date").addSnapshotListener { querySnapshot, error in
+            
+            self.messages = []
+            
+            if let e = error {
+                print("Could not retrieve messages from Firestore: \(e)")
+            } else {
+                if let snapshotDocument = querySnapshot?.documents {
+                    for doc in snapshotDocument {
+                        let data = doc.data()
+                        if let sender = data["sender"] as? String, let body = data["body"] as? String {
+                            let newMessage = Message(sender: sender, message: body)
+                            self.messages.append(newMessage)
+                            
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    @IBAction func sendPressed(_ sender: UIButton) {
+        
+        if let messageBody = messageTextField.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection("Messages").addDocument(data: ["sender": messageSender,
+                                                         "body": messageBody,
+                                                         "date": Date().timeIntervalSince1970
+                                                        ]
+            ) { error in
+                if let e = error {
+                    print("There was an issues adding message to Firestore: \(e)")
+                } else {
+                    print("Successfully saved message")
+                }
+            }
+        }
+    }
+    
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChatCell", for: indexPath) as! MessageCell
+        cell.messageLabel.text = messages[indexPath.row].message
+        return cell
+    }
+}
