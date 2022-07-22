3e883998f490eaff7b087a5c34af3ad0d057c7f9
Sibusiso94 <sybog1994@gmail.com>
2022-07-22 11:04:08 +0200

    Completed IQKeyboard setup

--- NotVega Help/AppDelegate.swift
+++ NotVega Help/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import FirebaseCore
 import FirebaseAuth
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
         
         return true
     }
