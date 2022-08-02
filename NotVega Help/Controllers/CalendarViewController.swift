be267eeeec90ea55f709e90eed67bbaefed80580
Tayla Roach <tayla.roach12@gmail.com>
2022-08-02 09:48:07 +0200

    Tweaked calendar UI, added other small ui changes

--- NotVega Help/Controllers/CalendarViewController.swift
+++ NotVega Help/Controllers/CalendarViewController.swift
@@ -63,6 +63,12 @@ class CalendarViewController: UIViewController, FSCalendarDelegate {
             }
         }
         
+        calendar.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.4).cgColor
+        calendar.layer.shadowOffset = CGSize(width: 10.0, height: 10.0)
+        calendar.layer.shadowOpacity = 0.2
+        calendar.layer.shadowRadius = 1.0
+        calendar.layer.cornerRadius = 10
+        
         loadSchedules(day: day)
         // Do any additional setup after loading the view.
     }
